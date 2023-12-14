4ec1ef702b7b6dd609812e66bb82553ed01f0752
andriiko <demigod03_blusher@icloud.com>
2023-12-13 20:17:12 -0500

    Initial Commit

---
+++ slopemeter/slopemeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  slopemeterApp.swift
+//  slopemeter
+//
+//  Created by Andrii Iliukhin on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct slopemeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
